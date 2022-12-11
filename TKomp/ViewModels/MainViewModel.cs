2a46bedde5a02d0be676d3a42825602946b5b18e
koniecdev <koniecdev@gmail.com>
2022-12-11 14:36:39 +0100

    Login form added

---
+++ TKomp/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+using TestWPF.Commands;
+
+namespace TKomp.ViewModels;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+	public MainViewModel()
+	{
+
+	}
+
+	
+
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+	private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+}
